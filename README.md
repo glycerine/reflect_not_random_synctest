c15534566f6b0e466688d540ae52fe964a352cad
Jason E. Aten, Ph.D. <jason@devnull>
2025-05-27 06:57:17 +0100

    red means red

--- README.md
+++ README.md
@@ -10,36 +10,28 @@ returns the 0 case as chosen.
 
 This does not happen when not under synctest.
 
-Transcript of runs. Note that green PASS is BAD here.
-
-It means that under synctest, reflect.Select returned
-case 0 on the first call 1000 times in a row.
+test logs:
 
 ~~~
 -*- mode: compilation; default-directory: "~/go/src/github.com/glycerine/reflect_not_random_synctest/" -*-
-Compilation started at Tue May 27 06:47:51
+Compilation started at Tue May 27 06:55:19
 
 GOTRACEBACK=all GOEXPERIMENT=synctest go test -v
 faketime = true
 === RUN   Test800_synctest_all_timers_dur_0_fire_now
---- PASS: Test800_synctest_all_timers_dur_0_fire_now (0.00s)
-PASS
-ok  	github.com/glycerine/reflect_not_random_synctest	0.161s
-
-Compilation finished at Tue May 27 06:47:51
-
+panic: in 10000 attempts, the first reflect.Select case was always 0
 ~~~
 
 ~~~
 -*- mode: compilation; default-directory: "~/go/src/github.com/glycerine/reflect_not_random_synctest/" -*-
-Compilation started at Tue May 27 06:47:27
+Compilation started at Tue May 27 06:56:10
 
-go test -v
+ go test -v
 faketime = false
 === RUN   Test800_synctest_all_timers_dur_0_fire_now
---- PASS: Test800_synctest_all_timers_dur_0_fire_now (0.00s)
+--- PASS: Test800_synctest_all_timers_dur_0_fire_now (0.20s)
 PASS
-ok  	github.com/glycerine/reflect_not_random_synctest	0.161s
+ok  	github.com/glycerine/reflect_not_random_synctest	0.305s
 
-Compilation finished at Tue May 27 06:47:28
+Compilation finished at Tue May 27 06:56:10
 ~~~
--- synctest_test.go
+++ synctest_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -16,46 +17,61 @@ func Test800_synctest_all_timers_dur_0_fire_now(t *testing.T) {
 	bubbleOrNot(func() {
 		// "SimNet using synctest depends on all the times set to duration 0/now firing before we quiese to durable blocking. verify this assumption under synctest. yes: note the Go runtime implementation does a select with a default: so it will discard the timer alert rather than block. Update: arg. no, the runtime does a special thing where it does not execute that select until it has a goro ready to accept it, so it always suceeds, I think.
 
-		t0 := time.Now()
-		//vv("start test800")
-		var timers []*time.Timer
-		N := 10
-		order := make(map[*time.Timer]int)
-		for range N {
-			ti := time.NewTimer(0)
-			order[ti] = len(timers)
-			timers = append(timers, ti)
+		// really? yep
+		firstCaseNonZeroSeen := false
+		attempts := 10000
+		for range attempts {
+
+			t0 := time.Now()
+			//vv("start test800")
+			var timers []*time.Timer
+			N := 10
+			order := make(map[*time.Timer]int)
+			for range N {
+				ti := time.NewTimer(0)
+				order[ti] = len(timers)
+				timers = append(timers, ti)
+			}
+
+			var cases []reflect.SelectCase
+			for _, ti := range timers {
+				cases = append(cases,
+					reflect.SelectCase{
+						Dir:  reflect.SelectRecv,
+						Chan: reflect.ValueOf(ti.C),
+					})
+			}
+			got := 0
+			for i, ti := range timers {
+				//  <-ti.C
+				chosen, recvVal, recvOK := reflect.Select(cases)
+				if !recvOK {
+					panic("why not recvOK ?")
+				}
+				//vv("on i=%v, chosen=%v, timer %v: %v", i, chosen, order[ti], recvVal)
+				_, _, _, _ = i, chosen, recvVal, ti
+				got++
+
+				if i == 0 && chosen != 0 {
+					firstCaseNonZeroSeen = true
+				}
+			}
+			now := time.Now()
+			if faketime {
+				if !t0.Equal(now) {
+					t.Fatalf("we have a problem, Houston. t0=%v, but now=%v", t0, now)
+				}
+				//vv("got %v timers firing now", got)
+				if got != N {
+					t.Fatalf("expected all N=%v timers to fire, not %v", N, got)
+				}
+			}
+			//vv("end test800") // shows same time as start, good.
 		}
 
-		var cases []reflect.SelectCase
-		for _, ti := range timers {
-			cases = append(cases,
-				reflect.SelectCase{
-					Dir:  reflect.SelectRecv,
-					Chan: reflect.ValueOf(ti.C),
-				})
+		if !firstCaseNonZeroSeen {
+			panic(fmt.Sprintf("in %v attempts, the first reflect.Select case was always 0", attempts))
 		}
-		got := 0
-		for i, ti := range timers {
-			//  <-ti.C
-			chosen, recvVal, recvOK := reflect.Select(cases)
-			if !recvOK {
-				panic("why not recvOK ?")
-			}
-			//vv("on i=%v, chosen=%v, timer %v: %v", i, chosen, order[ti], recvVal)
-			_, _, _, _ = i, chosen, recvVal, ti
-			got++
-		}
-		now := time.Now()
-		if faketime {
-			if !t0.Equal(now) {
-				t.Fatalf("we have a problem, Houston. t0=%v, but now=%v", t0, now)
-			}
-			//vv("got %v timers firing now", got)
-			if got != N {
-				t.Fatalf("expected all N=%v timers to fire, not %v", N, got)
-			}
-		}
-		//vv("end test800") // shows same time as start, good.
+
 	})
 }
